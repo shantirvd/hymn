93043f870f61b75fe222add88e2bac5436977f7d
juliedelarue <delaruejuulie@gmail.com>
2023-03-08 17:45:50 +0100

    Coding answer controller & view new & partial buzzer

--- app/policies/answer_policy.rb
+++ app/policies/answer_policy.rb
@@ -1,8 +1,16 @@
 class AnswerPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
   end
 end
