ccebe6ef26fe8034ed44ffd9ebde8babc00cb5d4
juliedelarue <delaruejuulie@gmail.com>
2023-03-09 14:15:00 +0100

    components style

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   resources :songs, only: %i[show] do
     resources :answers, only: %i[new create index]
   end
+
+  get 'components', to: 'pages#components', as: 'components'
+
 end
